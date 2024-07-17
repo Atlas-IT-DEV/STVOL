9722d51ef5b166d8b42b685b86548d91c1bdb22b
GravyTalk <deneaL123@mail.ru>
2024-07-17 03:56:39 +0300

    edit models.py

--- src/database/models.py
+++ src/database/models.py
@@ -5,36 +5,52 @@ from datetime import datetime
 
 
 class Image(BaseModel):
-    """Модель изображений"""
+    """
+    Модель изображений
+
+    "example": {
+
+        "url": "./src/public/bouquet\76f9f862-8202-493a-a753-fcde4..."
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     Url: Optional[StrictStr] = Field(..., alias="url")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "url": "./src/public/bouquet\76f9f862-8202-493a-a753-fcde4..."
-            }
-        }
-
 
 class CompanyData(BaseModel):
-    """Модель данных компании"""
+    """
+    Модель данных компании
+
+    "example": {
+
+        "name": "Зеленый Рэйв",
+
+        "description": "Зеленый Рэйв - это маленькая просторная зеленая комната,"
+                " в которой кальяны вмонтированы в зеленые стены"
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     Name: StrictStr = Field(..., alias="name")
     Disc: StrictStr = Field(..., alias="description")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "name": "Зеленый Рэйв",
-                "description": "Зеленый Рэйв - это маленькая просторная зеленая комната,"
-                        " в которой кальяны вмонтированы в зеленые стены"
-            }
-        }
-
 
 class User(BaseModel):
-    """Модель пользователя"""
+    """
+    Модель пользователя
+
+    "example": {
+
+        "name": "Porin229",
+
+        "telegram_id": 22,
+
+        "phone": "79128405592",
+
+        "count_bonus": 1000,
+
+        "referal": 0
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     Name: Optional[StrictStr] = Field(None, alias="name")
     TelegramID: StrictInt = Field(..., alias="telegram_id")
@@ -42,134 +58,139 @@ class User(BaseModel):
     CountBonus: Optional[StrictInt] = Field(0, alias="count_bonus")
     Ref: Optional[StrictInt] = Field(0, alias="referal")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "name": "Porin229",
-                "telegram_id": 22,
-                "phone": "79128405592",
-                "count_bonus": 1000,
-                "referal": 0
-            }
-        }
-
 
 class RefCodes(BaseModel):
-    """Модель реферальных кодов"""
+    """
+    Модель реферальных кодов
+
+    "example": {
+
+        "user_id": 2,
+
+        "code": "$2b$12$sWu02cjJ2Q3RHcMvuv08LOFeoU8lnWU8YXaF3XMOoECYdkrw.7ZxC"
+    }
+    """
     UserID: StrictInt = Field(..., alias="user_id")
     Code: StrictStr = Field(..., alias="code")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "user_id": 2,
-                "code": "$2b$12$sWu02cjJ2Q3RHcMvuv08LOFeoU8lnWU8YXaF3XMOoECYdkrw.7ZxC"
-            }
-        }
-
 
 class Order(BaseModel):
-    """Модель заказа"""
+    """
+    Модель заказа
+
+    "example": {
+
+        "user_id": 2,
+
+        "date": datetime.now(),
+
+        "total_price": 2340
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     UserID: StrictInt = Field(..., alias="user_id")
     Date: Optional[datetime] = Field(None, alias="date")
     TotalPrice: StrictInt = Field(..., alias="total_price")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "user_id": 2,
-                "date": datetime.now(),
-                "total_price": 2340
-            }
-        }
-
 
 class OrderBouquets(BaseModel):
-    """Модель букетов в каждом отдельном заказе"""
+    """
+    Модель букетов в каждом отдельном заказе
+
+    "example": {
+
+        "order_id": 2,
+
+        "bouquet_id": 9,
+
+        "quantity": 3
+    }
+    """
     OrderID: StrictInt = Field(..., alias="order_id")
     BouquetID: StrictInt = Field(..., alias="bouquet_id")
     Quantity: StrictInt = Field(..., alias="quantity")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "order_id": 2,
-                "bouquet_id": 9,
-                "quantity": 3
-            }
-        }
-
 
 
 class OrderHistory(BaseModel):
-    """Модель истории заказов"""
+    """
+    Модель истории заказов
+
+    "example": {
+
+        "order_id": 2,
+
+        "user_id": 9,
+
+        "date": datetime.now(),
+
+        "total_price": 2340,
+
+        "bouquets": [
+
+                  {
+                    "order_id": 1,
+
+                    "bouquet_id": 8,
+
+                    "quantity": 2
+                  }
+                ]
+    }
+    """
     OrderID: StrictInt = Field(..., alias="order_id")
     UserID: StrictInt = Field(..., alias="user_id")
     Date: Optional[datetime] = Field(..., alias="date")
     TotalPrice: StrictInt = Field(..., alias="total_price")
     Bouquets: list[OrderBouquets] = Field(..., alias="bouquets")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "order_id": 2,
-                "user_id": 9,
-                "date": datetime.now(),
-                "total_price": 2340,
-                "bouquets": [
-                          {
-                            "order_id": 1,
-                            "bouquet_id": 8,
-                            "quantity": 2
-                          }
-                        ]
-            }
-        }
-
 
 class BouquetsID(BaseModel):
-    """Модель словаря с айди букетами и их количеством"""
+    """
+    Модель словаря с айди букетами и их количеством
+
+    "example": {
+
+        "bouquet_id": 9,
+
+        "quantity": 3
+    }
+    """
     BouquetID: StrictInt = Field(..., alias="bouquet_id")
     Quantity: StrictInt = Field(..., alias="quantity")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "bouquet_id": 9,
-                "quantity": 3
-            }
-        }
-
 
 class Bouquet(BaseModel):
-    """Модель букета"""
+    """
+    Модель букета
+
+    "example": {
+
+        "name": "Букет красных роз",
+
+        "price": 9000,
+
+        "image_id": 3
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     Name: StrictStr = Field(..., alias="name")
     Price: StrictInt = Field(..., alias="price")
     ImageID: StrictInt = Field(..., alias="image_id")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "name": "Букет красных роз",
-                "price": 9000,
-                "image_id": 3
-            }
-        }
-
 
 class Adress(BaseModel):
-    """Модель адреса"""
+    """
+    Модель адреса
+
+    "example": {
+
+        "adress": "Улица Варашилова 9",
+
+        "user_id": 9
+    }
+    """
     ID: Optional[int] = Field(None, alias="id")
     Adress: StrictStr = Field(..., alias="adress")
     UserID: StrictInt = Field(..., alias="user_id")
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "adress": "Улица Варашилова 9",
-                "user_id": 9
-            }
-        }
 
