aeeceeb0aa75174b4d70cf00d00f182ff82888e1
GravyTalk <deneaL123@mail.ru>
2024-07-29 06:04:28 +0300

    fix user_services.py

--- src/service/user_services.py
+++ src/service/user_services.py
@@ -25,10 +25,10 @@ async def get_user_by_phone(user_phone: str):
 
 async def create_user(user: User):
     existing_user = await get_user_by_telegram_id(user.TelegramID)
-    if existing_user.TelegramID == user.TelegramID:
+    if existing_user and existing_user.TelegramID == user.TelegramID:
         raise HTTPException(status_code=404, detail="User already exist")
     existing_user = await get_user_by_phone(user.Phone)
-    if existing_user.Phone == user.Phone:
+    if existing_user and existing_user.Phone == user.Phone:
         raise HTTPException(status_code=404, detail="User already exist")
     user_id = await user_repository.create_user(user)
     return await get_user_by_id(user_id)
