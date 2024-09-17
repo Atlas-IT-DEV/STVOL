29ff22b252634fcbffd9184b2b403a3b0769234a
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-17 21:45:25 +0300

    fixed about page

--- src/pages/about_page/about_page.jsx
+++ src/pages/about_page/about_page.jsx
@@ -4,6 +4,7 @@ import logo from "../../images/logo.png";
 import cartIcon from "../../images/cart_icon.svg";
 import arrowWhite from "../../images/arrow_white.svg";
 import { useState } from "react";
+import BottomMenu from "../../components/bottom_menu/bottomMenu";
 
 const AboutPage = () => {
   const [isHide, setIsHide] = useState([false, false, false]);
@@ -32,99 +33,156 @@ const AboutPage = () => {
           </p>
         </div>
       </div>
-      <div
-        className={styles.buttonShowHide}
-        onClick={() => {
-          copyIsHide[0] = !copyIsHide[0];
-          setIsHide(copyIsHide);
-        }}
-      >
-        <p>Доставка</p>
-        <img
-          src={arrowWhite}
-          alt=""
-          className={isHide[0] == false ? styles.arrowDown : styles.arrowUp}
-        />
-      </div>
-      <div className={isHide[0] ? styles.viewOpen : styles.viewClose}>
-        <table>
-          <tr>
-            <th>Интервал</th>
-            <th>Москва</th>
-            <th>За МКАД</th>
-          </tr>
-          
-          <tr>
-            <th>Трехчасовой</th>
-            <th>Бесплатно</th>
-            <th>399+40₽/км</th>
-          </tr>
-          <tr>
-            <th>Часовой</th>
-          </tr>
-          <tr>
-            <th>Точное время</th>
-          </tr>
-        </table>
-      </div>
-      <div
-        className={styles.buttonShowHide}
-        onClick={() => {
-          copyIsHide[1] = !copyIsHide[1];
-          setIsHide(copyIsHide);
-        }}
-      >
-        <p>Возврат</p>
-        <img
-          src={arrowWhite}
-          alt=""
-          className={isHide[1] == false ? styles.arrowDown : styles.arrowUp}
-        />
-      </div>
-      <div
-        className={`${isHide[1] ? styles.viewOpen : styles.viewClose} ${
-          styles.deliveryView
-        }`}
-      >
-        <p>
-          В соответствии с Законом Российской Федерации «О защите прав
-          потребителей» от 07.02.1992 2300-1 (в ред. от 25.10.2007г.) и
-          Постановлением Правительства Российской Федерации от 19.01.1998 № 55
-          (в ред. 27.03.2007г.) срезанные цветы и горшечные растения обмену и
-          возврату не подлежат (Перечень непродовольственных товаров надлежащего
-          качества, не подлежащих возврату или обмену). <br />
-          <br />
-          Покупатель Интернет-магазина имеет право отказаться от получения
-          товара в момент его доставки, если доставленный товар ненадлежащего
-          качества (на основании п.3 ст. 497 ГК РФ, статья 21 Закона "О защите
-          прав потребителей"). Мы заботимся о повышении качества заказов и
-          рассматриваем все поступающие претензии в течение 24 часов с момента
-          доставки товара. По вопросам, касающимся выполнения заказов, вы можете
-          обращаться по электронной почте: ---- или к нам в телеграм: ----
-        </p>
-      </div>
-      <div
-        className={styles.buttonShowHide}
-        onClick={() => {
-          copyIsHide[2] = !copyIsHide[2];
-          setIsHide(copyIsHide);
-        }}
-      >
-        <p>Программа лояльности</p>
-        <img
-          src={arrowWhite}
-          alt=""
-          className={isHide[2] == false ? styles.arrowDown : styles.arrowUp}
-        />
-      </div>
-      <div className={isHide[2] ? styles.viewOpen : styles.viewClose}>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat
-          doloremque doloribus error architecto nostrum, nobis quos delectus,
-          nemo soluta aut possimus obcaecati hic eveniet assumenda, animi
-          numquam eaque debitis omnis.
-        </p>
-      </div>
+      <main>
+        <div
+          className={styles.buttonShowHide}
+          onClick={() => {
+            copyIsHide[0] = !copyIsHide[0];
+            setIsHide(copyIsHide);
+          }}
+        >
+          <p>Доставка</p>
+          <img
+            src={arrowWhite}
+            alt=""
+            className={isHide[0] == false ? styles.arrowDown : styles.arrowUp}
+          />
+        </div>
+        <div className={isHide[0] ? styles.viewOpen : styles.viewClose}>
+          <table>
+            <tr
+              style={{
+                color: "rgba(55, 55, 55, 1)",
+                fontSize: 22,
+                fontFamily: "TTNorms700",
+              }}
+            >
+              <td>Интервал</td>
+              <td>Москва</td>
+              <td>За МКАД</td>
+            </tr>
+
+            <tr
+              style={{
+                color: "rgba(156, 156, 156, 1)",
+                fontFamily: "TTNorms400",
+                fontSize: 22,
+                borderBottom: "1px solid rgba(55, 55, 55, 1)",
+                height: 45,
+              }}
+            >
+              <td>Трехчасовой</td>
+              <td>Бесплатно</td>
+              <td>399+40₽/км</td>
+            </tr>
+            <tr
+              style={{
+                color: "rgba(156, 156, 156, 1)",
+                fontFamily: "TTNorms400",
+                fontSize: 22,
+                borderBottom: "1px solid rgba(55, 55, 55, 1)",
+                height: 45,
+              }}
+            >
+              <td>Часовой</td>
+              <td>499 ₽</td>
+              <td>699+40₽/км</td>
+            </tr>
+            <tr
+              style={{
+                color: "rgba(156, 156, 156, 1)",
+                fontFamily: "TTNorms400",
+                fontSize: 22,
+                height: 45,
+              }}
+            >
+              <td>Точное время</td>
+              <td>999 ₽</td>
+              <td>1199+40₽/км</td>
+            </tr>
+          </table>
+        </div>
+        <div
+          className={styles.buttonShowHide}
+          onClick={() => {
+            copyIsHide[1] = !copyIsHide[1];
+            setIsHide(copyIsHide);
+          }}
+        >
+          <p>Возврат</p>
+          <img
+            src={arrowWhite}
+            alt=""
+            className={isHide[1] == false ? styles.arrowDown : styles.arrowUp}
+          />
+        </div>
+        <div
+          className={`${isHide[1] ? styles.viewOpen : styles.viewClose} ${
+            styles.deliveryView
+          }`}
+        >
+          <p>
+            В соответствии с Законом Российской Федерации «О защите прав
+            потребителей» от 07.02.1992 2300-1 (в ред. от 25.10.2007г.) и
+            Постановлением Правительства Российской Федерации от 19.01.1998 № 55
+            (в ред. 27.03.2007г.) срезанные цветы и горшечные растения обмену и
+            возврату не подлежат (Перечень непродовольственных товаров
+            надлежащего качества, не подлежащих возврату или обмену). <br />
+            <br />
+            Покупатель Интернет-магазина имеет право отказаться от получения
+            товара в момент его доставки, если доставленный товар ненадлежащего
+            качества (на основании п.3 ст. 497 ГК РФ, статья 21 Закона "О защите
+            прав потребителей"). Мы заботимся о повышении качества заказов и
+            рассматриваем все поступающие претензии в течение 24 часов с момента
+            доставки товара. По вопросам, касающимся выполнения заказов, вы
+            можете обращаться по электронной почте: ---- или к нам в телеграм:
+            ----
+          </p>
+        </div>
+        <div
+          className={styles.buttonShowHide}
+          onClick={() => {
+            copyIsHide[2] = !copyIsHide[2];
+            setIsHide(copyIsHide);
+          }}
+        >
+          <p>Программа лояльности</p>
+          <img
+            src={arrowWhite}
+            alt=""
+            className={isHide[2] == false ? styles.arrowDown : styles.arrowUp}
+          />
+        </div>
+        <div
+          className={`${isHide[2] ? styles.viewOpen : styles.viewClose} ${
+            styles.loyalityView
+          }`}
+        >
+          <div>
+            <div className={styles.descriptionLoyality}>
+              <p className={styles.nameText}>Бронзовый STVOL</p>
+              <p className={styles.discountText}>5%</p>
+            </div>
+            <p className={styles.priceText}>15 000 ₽ - 49 999 ₽</p>
+          </div>
+          <div>
+            <div className={styles.descriptionLoyality}>
+              <p className={styles.nameText}>Серебряный STVOL</p>
+              <p className={styles.discountText}>7%</p>
+            </div>
+            <p className={styles.priceText}>50 000 ₽ - 74 999 ₽</p>
+          </div>
+          <div>
+            <div className={styles.descriptionLoyality}>
+              <p className={styles.nameText}>Золотой STVOL</p>
+              <p className={styles.discountText}>10%</p>
+            </div>
+            <p className={styles.priceText}>75 000 ₽ - 100 000 ₽</p>
+          </div>
+        </div>
+      </main>
+
+      <BottomMenu />
     </div>
   );
 };
