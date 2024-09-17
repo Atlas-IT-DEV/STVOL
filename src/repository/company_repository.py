a15f1515f0cade16d324d4d82cd3f0dc762d33d5
GravyTalk <deneaL123@mail.ru>
2024-09-18 01:55:52 +0300

    fix name table

--- src/repository/company_repository.py
+++ src/repository/company_repository.py
@@ -4,28 +4,28 @@ from src.database.my_connector import db
 
 
 def get_all_companys():
-    query = "SELECT * FROM сompany_Data"
+    query = "SELECT * FROM сompany_data"
     return db.fetch_all(query)
 
 
 def get_company_by_id(company_id: int):
-    query = "SELECT * FROM сompany_Data WHERE id=%s"
+    query = "SELECT * FROM сompany_data WHERE id=%s"
     return db.fetch_one(query, (company_id,))
 
 
 def create_company(company: CompanyData):
-    query = "INSERT INTO сompany_Data (name, description) VALUES (%s, %s)"
+    query = "INSERT INTO сompany_data (name, description) VALUES (%s, %s)"
     params = (company.Name, company.Disc)
     cursor = db.execute_query(query, params)
     return cursor.lastrowid
 
 
 def update_company(company_id: int, company: CompanyData):
-    query = "UPDATE сompany_Data SET name=%s, description=%s WHERE id=%s"
+    query = "UPDATE сompany_data SET name=%s, description=%s WHERE id=%s"
     params = (company.Name, company.Disc, company_id)
     db.execute_query(query, params)
 
 
 def delete_company(company_id: int):
-    query = "DELETE FROM сompany_Data WHERE id=%s"
+    query = "DELETE FROM сompany_data WHERE id=%s"
     db.execute_query(query, (company_id,))
