bf4972900e899612dba06dfd259cb99989c2fd54
Clourincess <96530726+Clourincess@users.noreply.github.com>
2024-09-17 23:58:01 +0300

    added product card component

---
+++ src/components/product_card/product_card.jsx
@@ -0,0 +1,49 @@
+import styles from "./product_card.module.css";
+
+import bouqet from "../../images/buket1.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import "swiper/css/navigation";
+import { FreeMode, Pagination } from "swiper/modules";
+
+const ProductCard = () => {
+  return (
+    <div className={styles.container}>
+      <Swiper
+        style={{
+          "--swiper-pagination-color": "rgba(237, 237, 237, 1)",
+          "--swiper-pagination-bullet-inactive-color": "rgba(131, 131, 131, 1)",
+          "--swiper-pagination-bullet-inactive-opacity": "1",
+          "--swiper-pagination-bullet-size": "8px",
+          "--swiper-pagination-bullet-horizontal-gap": "3px",
+        }}
+        className={styles.slideTrack}
+        modules={[FreeMode, Pagination]}
+        spaceBetween={50}
+        freeMode={false}
+        pagination={true}
+      >
+        <SwiperSlide className={styles.slider}>
+          <img src={bouqet} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slider}>
+          <img src={bouqet} alt="" />
+        </SwiperSlide>
+        <SwiperSlide className={styles.slider}>
+          <img src={bouqet} alt="" />
+        </SwiperSlide>
+      </Swiper>
+      <p className={styles.nameProductText}>Букет из роз, фиалок и ромашек</p>
+      <div className={styles.priceView}>
+        <p className={styles.priceText}>3050 ₽</p>
+        <p className={styles.oldPriceText}>3050₽</p>
+        <p className={styles.discountText}>-15%</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
--- src/pages/catalog_page/catalog_page.jsx
+++ src/pages/catalog_page/catalog_page.jsx
@@ -1,12 +1,133 @@
 import Header from "../../components/header/header";
 import styles from "./catalog_page.module.css";
+import sortIcon from "../../images/sort_icon.svg";
+import filterIcon from "../../images/filter_icon.svg";
+import { useState } from "react";
+import BottomMenu from "../../components/bottom_menu/bottomMenu";
+import ProductCard from "../../components/product_card/product_card";
 
 const CatalogPage = () => {
+  const [isPressed, setIsPressed] = useState([
+    [true],
+    [false, false, false, false, false, false],
+  ]);
+  let copyIsPressed = Array.from(isPressed);
   return (
     <div className={styles.container}>
       <div className={styles.header}>
         <Header />
       </div>
+      <p className={styles.namePageText}>Каталог</p>
+
+      <div className={styles.SortFilterButtons}>
+        <div className={styles.sortButton}>
+          <img src={sortIcon} alt="" />
+        </div>
+        <div
+          className={styles.filterButton}
+          onClick={() => {
+            copyIsPressed[0][0] = !copyIsPressed[0][0];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          <img src={filterIcon} alt="" />
+        </div>
+      </div>
+
+      <div
+        className={isPressed[0][0] ? styles.filtersOpen : styles.filtersClose}
+      >
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][0]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][0] = !copyIsPressed[1][0];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Розы
+        </p>
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][1]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][1] = !copyIsPressed[1][1];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Тюльпаны
+        </p>
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][2]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][2] = !copyIsPressed[1][2];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Пионы
+        </p>
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][3]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][3] = !copyIsPressed[1][3];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Орхидеи
+        </p>
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][4]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][4] = !copyIsPressed[1][4];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Пионы
+        </p>
+        <p
+          className={`${styles.filterButtons} ${
+            isPressed[1][5]
+              ? styles.activeFilterButton
+              : styles.inActiveFilterButton
+          }`}
+          onClick={() => {
+            copyIsPressed[1][5] = !copyIsPressed[1][5];
+            setIsPressed(copyIsPressed);
+          }}
+        >
+          Герберы
+        </p>
+      </div>
+      <div className={styles.products}>
+        <div className={styles.typeOfBouqets}>
+          <p className={styles.nameFilterText}>Летние букеты</p>
+          <div className={styles.productsView}>
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </div>
+        </div>
+      </div>
+      <BottomMenu />
     </div>
   );
 };
